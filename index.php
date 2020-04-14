0c7540697ee8b1a3e49a9914462e0639f1155986
Ashley Abbott <ashley.abbott@eagleeye.com>
2020-04-14 11:34:42 +0100

    testing build

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $app->add(new \Slim\Middleware\HttpBasicAuthentication(array(
 // grouping the /api route, see Slim's group() method documentation for more
 $app->group('/api', function () use ($app) {
 
-    $data = ['This api has been deployed automatically by cloud build', 123456789];
+    $data = ['And again', 9876543210];
 
     // api route "test" which just gives back some demo data
     $app->get('/test', function ($request, $response, $args) use ($data) {
